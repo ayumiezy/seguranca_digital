f32c169eab155d30c549d54a6f4df17b93bedd4f
ayumi <105938742+ayumiezy@users.noreply.github.com>
2024-07-12 13:24:30 -0300

    console

--- main.js
+++ main.js
@@ -12,12 +12,14 @@ function diminuiTamanho() {
         tamanhoSenha--;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
 function aumentaTamanho(){
     if(tamanhoSenha < 20){
         tamanhoSenha++;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
 
 const campoSenha = document.querySelector('#campo-senha');
@@ -25,9 +27,12 @@ const campoSenha = document.querySelector('#campo-senha');
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 function geraSenha(){
-    let numeroAleatorio = Math.random()*letrasMaiusculas.length;
-    numeroAleatorio = Math.floor(numeroAleatorio);
-    console.log(letrasMaiusculas[numeroAleatorio]);
+
+    for(let i = 0; i < tamanhoSenha;i++){
+        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        console.log(letrasMaiusculas[numeroAleatorio]);
+    }
 }
 
 campoSenha.value = letrasMaiusculas;
